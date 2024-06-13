fd9298595cb58b9abc4807028c97517d4b0e7906
Avishkar-kale <152062686+Avishkar-kale@users.noreply.github.com>
2024-06-13 18:24:32 +0530

    Add files via upload

---
+++ a_exe4.py
@@ -0,0 +1,14 @@
+string = input("Enter a string")
+char_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+             'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+var = 0
+total = 0
+for char in string:
+    if char in char_list:
+        position = string.index(char,var)
+        # print(position)aaa
+        var = position+1
+        total = total + position
+print(total)
